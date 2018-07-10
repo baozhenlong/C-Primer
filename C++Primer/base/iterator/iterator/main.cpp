48259f4cacae7a07d30acb87b25f145f891d9eca
bzl <mac@macdeMacBook-Pro.local>
2018-07-10 16:19:49 +0800

    迭代器部分

---
+++ C++Primer/base/iterator/iterator/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  iterator
+//
+//  Created by mac on 2018/7/9.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+//迭代器
+//所有标准库容器都可以使用迭代器
+//类似于指针类型，迭代器也提供了对对象的间接访问
+//有效的迭代器或者指向某个元素，或者指向容器中尾元素的下一位置；其他情况都属于无效
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    //---使用迭代器
+    //有迭代器的类型同时拥有返回迭代器的成员
+    //v.begin()---v的第一个元素
+    //v.end()
+    
+    return 0;
+}
